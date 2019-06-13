5b11984993e2bcfb0eb81386235fcf781d57c855
sewoo <sewooimda@nate.com>
2019-06-13 16:46:03 +0900

    버그수정

--- exchange-rate-support/src/test/java/com/wirbarley/assignment/support/util/FormatUtilTest.java
+++ exchange-rate-support/src/test/java/com/wirbarley/assignment/support/util/FormatUtilTest.java
@@ -1,6 +1,5 @@
 package com.wirbarley.assignment.support.util;
 
-import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,12 +7,12 @@ import java.math.BigDecimal;
 
 import static org.junit.Assert.*;
 
-@Slf4j
+
 @SpringBootTest
 public class FormatUtilTest {
 
 	@Test
 	public void wirbarleyBigDicimalFormat() {
-		log.debug (FormatUtil.wirbarleyBigDicimalFormat(new BigDecimal ("123123.123")));
+		System.out.println(FormatUtil.wirbarleyBigDicimalFormat(new BigDecimal ("123123.123")));
 	}
 }
---
+++ exchange-rate-web/build/resources/main/static/assets/vendor/js/common.js
@@ -0,0 +1,86 @@
+$(document).ready(function() {
+    var rateUrl = "/v1/exchange-rate/currency?currency=";
+    var calUrl = "/v1/exchange-rate/currency/calculation";
+
+    $("#result_area").hide();
+
+    $('#recipient-country').change(function () {
+        var currency = $(this).val();
+        $.ajax({
+            url: rateUrl + currency,
+            type:"GET",
+            success : function (data) {
+                var map = data.quotes;
+                $('#exchange-rate').text(numberWithCommas(resultMap(map,"USD"+currency)) + " " + currency + "/USD");
+            },
+            error : function (e) {
+                console.log(e.responseText);
+            }
+        });
+
+    });
+
+    $('#submit').on('click',function() {
+
+        var currency = $("#recipient-country option:selected").val();
+        var price = $("#remittance-amount").val();
+
+        if(!validation(currency, price)) return false;
+
+        $.ajax({
+            url: calUrl,
+            type:"POST",
+            data:{"currency":currency,"price":price},
+            success : function (data) {
+                $("#result_area").show();
+                $("#amount-received").text(data.price);
+                $("#currency").text(data.currencyType);
+            },
+            error : function (e) {
+                console.log(e.responseText);
+            }
+        });
+    });
+
+
+});
+
+var resultMap = function(map,k) {
+    return map[k];
+}
+
+var numberWithCommas = function(x) {
+    var y = Math.floor(x*100)/100;
+    y = y.toFixed(2);
+    return y.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+var validation = function(currency, price) {
+    var i = 0;
+    $('#recipient-country').each(function(index){
+        if($(this).val() == currency) {
+            i++;
+        }
+    });
+
+    if (i <= 0) {
+        alert("수취국가를 선택하셔야 합니다.");
+        $('#recipient-country').focus();
+        return false;
+    }
+
+    price = parseInt(price);
+
+    /*if (price <= 0 || price > 10000) {
+        alert("송금액(USD)이 바르지 않습니다.");
+        $("#remittance-amount").focus();
+        return false;
+    }*/
+
+    return true;
+}
+
+
+
+
+
